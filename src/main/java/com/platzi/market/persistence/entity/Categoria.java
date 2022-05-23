ac5005a1cb708e2ab412aee18b8f3e3d7b1c0dd0
Felipe <fquiceno1@misena.edu.co>
2022-05-23 16:40:39 -0500

    [CREATE],Mapeando tablas como clases, Column con el nombre de la tabla real, le damos solo a las columnas que cambian de nombre en la clase,El id del cliente es la cedula por eso no autoincrementa, Table tambien se pone cuando cambian los nombres de las tablas en la base de datos y en las clases.

---
+++ src/main/java/com/platzi/market/persistence/entity/Categoria.java
@@ -0,0 +1,41 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categorias")
+    private Long idCategoria;
+
+    private String descripcion;
+
+    private boolean estado;
+
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
