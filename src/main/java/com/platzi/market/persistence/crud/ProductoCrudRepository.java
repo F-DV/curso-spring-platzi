83cee7152165f9a0b6602d07298e700f558d25fa
Felipe <fquiceno1@misena.edu.co>
2022-05-23 19:14:13 -0500

    [CREATE],Hacemos uso de las funciones que nos provee CrudRepository, ademas creamos Query Methods para consultas mucho mas especializadas que no nos provee CrudRepository,Utilizamos la anotacion @Repository para indicarle a Spring que se trata del componente que interactura directamente con la base de datos, en application.properties agregamos una line para ver como Spring realiza las consultas mas detalladamente

---
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,19 @@
+package com.platzi.market.persistence.crud;
+
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto,Long> {
+
+    List<Producto> findByIdCategoria(Long idCategoria);
+
+    List<Producto>  findByCategoriaOrderByNombreAsc(Long idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+
+}
